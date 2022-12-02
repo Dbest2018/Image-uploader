8889c0dfddede89bdebfc2c995e3680fcad53353
Dam <shit2wice@gmail.com>
2022-12-02 19:59:44 +0100

    feat: Add home page

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,26 @@
+// import { useState } from "react";
+import "./Home.css";
+import image from "../assets/image.svg";
+
+const Home = () => {
+  // const [image, setImage] = useState("");
+
+  // const handleChange = (e) => {
+  //   setImage(e.target.files);
+  // };
+
+  return (
+    <div className="home">
+      <div className="home__title">Upload your image</div>
+      <div className="home__text">File should be Jpeg, Png,..</div>
+      <div className="image-container">
+        <img src={image} alt="placholder" />
+        <div className="option-text">Drag & Drop your image</div>
+      </div>
+      <div className="option-text">Or</div>
+      <button className="home__button">Choose a file</button>
+    </div>
+  );
+};
+
+export default Home;
