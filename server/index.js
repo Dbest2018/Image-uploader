38561ec6bc23aafe044e9c155eea0050a7aee6ff
Dam <shit2wice@gmail.com>
2022-12-04 14:15:06 +0100

    feat: Create upload route on server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 import express from "express";
+import fileUpload from "express-fileupload";
 
 const app = express();
+app.use(fileUpload());
 
-app.get("/", (req, res) => {
-  res.send("Welcome home");
+app.post("/upload", (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: "No file uploaded" });
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
 });
 
 const port = process.env.PORT || 5000;
